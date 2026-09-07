7790e395a3e343b5b9ddaaad03de55b6c34c401a
Nyrox <smtp.nypay@gmail.com>
2026-09-07 12:29:37 +0400

    Update script URLs to use NyroxYT repository

--- wings/run.sh
+++ wings/run.sh
@@ -178,8 +178,8 @@ while true; do
     
     case $opt in
         1) ssl_setup ;;
-        2) bash <(curl -fsSL https://raw.githubusercontent.com/nobita329/Nobita-Cloud/refs/heads/main/wings/install.sh) ;;
-        3) bash <(curl -fsSL https://raw.githubusercontent.com/nobita329/Nobita-Cloud/refs/heads/main/wings/mang.sh) ;;
+        2) bash <(curl -fsSL https://raw.githubusercontent.com/NyroxYT/NyroxHub/refs/heads/main/wings/install.sh) ;;
+        3) bash <(curl -fsSL https://raw.githubusercontent.com/NyroxYT/NyroxHub/refs/heads/main/wings/mang.sh) ;;
         4) bash <(curl -fsSL https://raw.githubusercontent.com/nobita329/ptero/refs/heads/main/ptero/wings/db.sh) ;;
         5) uninstall_menu ;;
         0) 
